cd575245287d3e97ba1285df616dce0d54a845c9
Zhang Wen <zhangwen1932@hotmail.com>
2018-12-28 17:46:33 +0800

    项目初始化

---
+++ app/containers/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class IndexApp extends Component {
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    );
+  }
+}
+
+export default IndexApp;
--- app/index.js
+++ app/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+
+import IndexApp from './containers';
 
 const div = document.createElement('div');
 div.setAttribute('id', 'app');
 document.body.appendChild(div);
 
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('app'),
+const mountNode = document.getElementById('app');
+
+
+render(
+  <IndexApp />,
+  mountNode,
 );
