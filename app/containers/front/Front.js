dae06b6fb090ff69a991d18f75463c8460f47724
Zhang Wen <zhangwen1932@hotmail.com>
2019-01-15 17:28:06 +0800

    /添加前端路由和管理页路由

---
+++ app/containers/front/Front.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Front extends Component {
+  render() {
+    return (
+      <h1>Front Page</h1>
+    );
+  }
+}
+
+export default Front;
--- app/containers/index.js
+++ app/containers/index.js
@@ -1,53 +1,25 @@
 import React, { Component } from 'react';
-import { Form, Input, Button } from 'antd';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { actions } from '../reducers';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import Admin from './admin/Admin';
+import Front from './front/Front';
 
 class IndexApp extends Component {
-  handleSubmit = (e) => {
-    e.preventDefault();
-    // eslint-disable-next-line react/destructuring-assignment
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        // eslint-disable-next-line react/destructuring-assignment
-        this.props.sendInfo(values.testdata);
-      }
-    });
-  }
-
   render() {
-    // eslint-disable-next-line react/destructuring-assignment
-    const { getFieldDecorator } = this.props.form;
     return (
-      <Form layout="inline" onSubmit={this.handleSubmit} style={{ margin: 15 }}>
-        <Form.Item>
-          {getFieldDecorator('testdata', {
-            rules: [{ required: true, message: '请输入一段文字' }],
-          })(
-            <Input placeholder="Test text" />,
-          )}
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">Click</Button>
-        </Form.Item>
-      </Form>
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/admin" component={Admin} />
+            <Route component={Front} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    test: state.globalState.test,
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    sendInfo: bindActionCreators(actions.send_test, dispatch),
-  };
-}
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(Form.create()(IndexApp));
+export default IndexApp;
