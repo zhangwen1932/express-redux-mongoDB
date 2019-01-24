c9744e2526d00a9bf5626f6af2b51681528588d9
Zhang Wen <zhangwen1932@hotmail.com>
2019-01-24 10:53:51 +0800

    设置过期时间

--- server/api/apiServer.js
+++ server/api/apiServer.js
@@ -24,7 +24,7 @@ app.use(session({
   secret: 'express_react_cookie', // 对session_id相关的cookie进行签名
   resave: true,
   saveUninitialized: true, // 是否保存初始化的会话
-  cookie: { maxAge: 60 * 1000 }, // 过期时间 单位毫秒
+  cookie: { maxAge: 60 * 1000 * 30 }, // 过期时间 单位毫秒
 }));
 
 // 测试接口是否有问题
