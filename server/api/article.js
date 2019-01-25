456dcdfc993706df1a07a7e33ea2b4d288c9c959
Zhang Wen <zhangwen1932@hotmail.com>
2019-01-25 16:01:07 +0800

    添加后端文章接口

--- server/api/article.js
+++ server/api/article.js
@@ -6,7 +6,6 @@ const router = Express.Router();
 
 router.post('/publishArticle', (req, res) => {
   const { title, content } = req.body;
-  console.log(req.body);
   if (!title) {
     responseClient(res, 200, 2, '标题不可为空');
     return;
@@ -23,7 +22,6 @@ router.post('/publishArticle', (req, res) => {
     if (err) {
       console.log('Error:' + err);
     } else {
-      console.log(res);
       responseClient(res, 200, 0, '文章发表成功');
     }
   });
