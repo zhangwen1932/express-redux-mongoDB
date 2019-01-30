efee9bf944117cfe7fe09a951023b6778f4d9d65
Zhang Wen <zhangwen1932@hotmail.com>
2019-01-30 20:22:29 +0800

    文章详情接口

--- server/api/article.js
+++ server/api/article.js
@@ -49,4 +49,15 @@ router.get('/getArticles', (req, res) => {
     });
 });
 
+router.get('/getArticlesDetail', (req, res) => {
+  console.log('req.query', req.query);
+  const { id } = req.query;
+  Article.findOne({ _id: id }, '_id title content isPublish')
+    .then((result) => {
+      responseClient(res, 200, 0, 'success', result);
+    }).cancel((err) => {
+      console.log('err', err);
+    });
+});
+
 module.exports = router;
