fcb504b260f9771830ef9d2fe51dab0970be6058
Zhang Wen <zhangwen1932@hotmail.com>
2019-02-11 15:04:35 +0800

    恢复到以前的界面

--- app/containers/front/Front.js
+++ app/containers/front/Front.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
-import Nav from './components/nav/Nav';
-import Banner from './components/banner/Banner';
-import Content from './components/content/Content';
-import Footer from './components/footer/Footer';
 
-export default class Home extends Component {
+class Front extends Component {
   render() {
     return (
-      <div>
-        <Nav />
-        <Banner />
-        <Content />
-        <Footer />
-      </div>
+      <h1>Front Page</h1>
     );
   }
 }
+
+export default Front;
